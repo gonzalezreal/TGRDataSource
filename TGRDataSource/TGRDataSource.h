ed335b6fb710237eba0f4b6e4929446ef9f6dc9b
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2014-04-18 17:20:22 +0200

    Initial Commit

---
+++ TGRDataSource/TGRDataSource.h
@@ -0,0 +1,13 @@
+//
+//  TGRDataSource.h
+//  TGRDataSource
+//
+//  Created by guille on 18/04/14.
+//  Copyright (c) 2014 Guillermo Gonzalez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TGRDataSource : NSObject
+
+@end
---
+++ TGRDataSource/TGRDataSource.m
@@ -0,0 +1,13 @@
+//
+//  TGRDataSource.m
+//  TGRDataSource
+//
+//  Created by guille on 18/04/14.
+//  Copyright (c) 2014 Guillermo Gonzalez. All rights reserved.
+//
+
+#import "TGRDataSource.h"
+
+@implementation TGRDataSource
+
+@end
